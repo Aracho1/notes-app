f8727b460a54f55db29605ac9ff444054352506c
AJ <aj.monty@gmail.com>
2021-01-19 18:35:17 +0000

    added some listing functionality

--- views/interface.js
+++ views/interface.js
@@ -1,9 +1,10 @@
-var note = new Note;
+var notebook = [];
 document.getElementById("create-note").onclick = function() {
 
-
+    var note = new Note;
     var noteText = document.getElementById("myText").value;
     note.create(noteText);
+    notebook[note.getNoteTitle()] = note.getNoteText()
     var entry = document.createElement('li')
     var tag = document.createElement('a')
     let noteTitle = note.getNoteTitle();
@@ -16,7 +17,14 @@ document.getElementById("create-note").onclick = function() {
 showFullNoteOnClick()
 function showFullNoteOnClick(){
   window.addEventListener("hashchange", function(){
-    console.log("hello")
+    var hash = this.location.hash
+    var hashToString = hash.replace(/\%20/g, ' ');
+    var string = hashToString.replace('#', '');
+    console.log(notebook)
+    console.log(notebook[string])
+    var element = document.createElement('p');
+    element.appendChild(document.createTextNode(notebook[string]));
+    document.getElementById("content").appendChild(element);
   });
 };
 
