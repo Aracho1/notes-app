6c3836738f2bd267ac5e4a6f4a92a67f07ff2a76
Aman Tank <amantank187@gmail.com>
2021-01-19 17:10:31 +0000

    Creating list with notes

--- views/interface.js
+++ views/interface.js
@@ -1,6 +1,39 @@
+var note = new Note;
 document.getElementById("create-note").onclick = function() {
-    var note = document.getElementById("myText").value; 
-    var entry = document.createElement('li') 
-    entry.appendChild(document.createTextNode(note))
-    document.getElementById("list").appendChild(entry);
+
+
+    var noteText = document.getElementById("myText").value;
+    note.create(noteText);
+    var entry = document.createElement('li')
+    var tag = document.createElement('a')
+    let noteTitle = note.getNoteTitle();
+    tag.setAttribute("id",`${noteTitle}`);
+    tag.setAttribute("href",`#${noteTitle}`);
+    tag.appendChild(document.createTextNode(noteTitle));
+    tag.appendChild(entry);
+    document.getElementById("list").appendChild(tag);
 }
+showFullNoteOnClick()
+function showFullNoteOnClick(){
+  window.addEventListener("hashchange", function(){
+    console.log("hello")
+  });
+};
+
+// function showCurrentNote (){
+//   showNote(getNoteFromUrl(window.location));
+// };
+
+// function getNoteFromUrl(`#${noteTitle}`){
+//   return location.hash.split("#")[1];
+// };
+//
+// function showNote(note){
+//   document
+//   .getElementById("content")
+//   .innerHTML = note.getNoteText();
+// };
+
+// document.getElementById("content").onclick = function() {
+//
+// }
