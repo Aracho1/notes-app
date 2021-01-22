506fc34e7fb75cbd0edd00e88ebd0475918f3498
Aman Tank <amantank187@gmail.com>
2021-01-22 14:13:42 +0000

    Added more tests

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -12,6 +12,10 @@ it('note is an instance of Note', function() {
 
 myNote.create(1, "Here is my first note")
 
+it('It assigns the ID when the note is created', function(){
+  expect(myNote.noteId).toEqual(1)
+});
+
 it('getNoteText() returns the note text', function() {
   expect(myNote.getNoteText()).toEqual("Here is my first note")
 });
@@ -19,3 +23,8 @@ it('getNoteText() returns the note text', function() {
 it('getNoteTiTle() returns the first 20 characters', function() {
   expect(myNote.getNoteTitle()).toEqual("Here is my first not...")
 });
+
+it('It should be able to truncate the the string to 20 characters',function(){
+  expect(myNote.truncate("Here is my first note")).toEqual("Here is my first not...")
+});
+
